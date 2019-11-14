59f4189e088c7ebc5dbf188a2e7917c882aef483
Ivanq <imachug@yandex.ru>
2019-11-14 23:24:55 +0300

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sslcrypto",
-    version="1.0",
+    version="1.1",
     description="ECIES, AES and RSA OpenSSL-based implementation with fallback",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
