cfa3feccdcf450e103092eaf930c6bd8112826d9
Ivanq <imachug@yandex.ru>
2019-11-16 22:28:05 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="sslcrypto",
-    version="1.5",
+    version="1.6",
     description="ECIES, AES and RSA OpenSSL-based implementation with fallback",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Ivanq",
     author_email="imachug@gmail.com",
     url="https://github.com/imachug/sslcrypto",
-    packages=["sslcrypto"],
+    packages=find_packages(),
     install_requires=[
         "pyaes==1.6.1",
         "base58check==1.0.2"
