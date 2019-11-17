2f5c020376813b0ccd4e4cb6e27807ec7ecb1b25
Ivanq <imachug@yandex.ru>
2019-11-17 11:51:24 +0300

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sslcrypto",
-    version="1.10",
+    version="1.11",
     description="ECIES, AES and RSA OpenSSL-based implementation with fallback",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
