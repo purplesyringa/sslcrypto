273060033b61a728d636e4243e5f0720c7c06d4b
Ivanq <imachug@yandex.ru>
2019-12-03 21:26:32 +0300

    Move some rules from pylintrc to files

--- sslcrypto/fallback/rsa.py
+++ sslcrypto/fallback/rsa.py
@@ -1,3 +1,5 @@
+# pylint: disable=too-few-public-methods
+
 class RSA:
     def get_backend(self):
         return "fallback"
--- sslcrypto/openssl/rsa.py
+++ sslcrypto/openssl/rsa.py
@@ -1,3 +1,5 @@
+# pylint: disable=too-few-public-methods
+
 from .library import openssl_backend
 
 
