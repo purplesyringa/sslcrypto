d657ff618abce7115c4670b7ccb31d8d82c668fa
Ivanq <imachug@yandex.ru>
2019-12-11 18:52:18 +0300

    Make conf.parallelize catch exceptions

--- test/conf.py
+++ test/conf.py
@@ -4,10 +4,15 @@ import threading
 def parallelize(cnt):
     def decorator(f):
         def decorated(*args, **kwargs):
-            def run():
-                f(*args, **kwargs)
-
             threads = []
+            exception = None
+
+            def run():
+                try:
+                    f(*args, **kwargs)
+                except BaseException as e:
+                    exception = e
+
             for _ in range(cnt):
                 thread = threading.Thread(target=run)
                 thread.daemon = True
@@ -15,6 +20,9 @@ def parallelize(cnt):
                 threads.append(thread)
             for thread in threads:
                 thread.join()
+
+            if exception is not None:
+                raise exception
 
         decorated.__name__ = f.__name__
         return decorated
