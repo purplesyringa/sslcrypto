c829d20599339f0f567dfae5f4dedd26311790c4
Ivanq <imachug@yandex.ru>
2020-04-08 23:42:48 +0300

    v5.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sslcrypto",
-    version="5.2",
+    version="5.3",
     description="ECIES, AES and RSA OpenSSL-based implementation with fallback",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
